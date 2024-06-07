5cd6b51dc82b709115d9e1519cc38bbde7f512ab
kavya-246 <kavyamurali090@gmail.com>
2024-06-07 15:10:48 +0530

    website created

--- src/components/Viewall.jsx
+++ src/components/Viewall.jsx
@@ -1,33 +1,10 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 const Viewall = () => {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-      <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/add">Add</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/search">Search</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/delete">Delete</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/viewall">Viewall</a>
-        </li>
-        
-      </ul>
-    </div>
-  </div>
-</nav>
+        <NavBar/>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
